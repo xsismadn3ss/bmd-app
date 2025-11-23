ded160c3415dddc2b5f3eb6be0904ec41e285ef8
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-22 21:47:42 -0600

    ♻️  incluir ruta /auth a la plantilla base

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,7 @@ function RootLayoutInner() {
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen
           name="settings"
           options={{ presentation: "fullScreenModal", title: "Ajustes" }}
