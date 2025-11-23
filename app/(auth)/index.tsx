4d121a08961ad49994b84bbe3706692ea3ed0dba
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-22 22:47:41 -0600

    🚸  agregar formulario de login y hacer vista adaptable al teclado

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,37 +1,57 @@
 import LoginForm from "@/components/auth/login-form";
-import { ThemedText } from "@/components/themed-text";
-import { ThemedView } from "@/components/themed-view";
+import RegisterForm from "@/components/auth/register-form";
+import { useThemeColor } from "@/hooks/use-theme-color";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { useTheme } from "@react-navigation/native";
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
 
 export default function AuthHomeScreen(): React.JSX.Element {
   const { colors } = useTheme();
+  const backgroundColor = useThemeColor(
+    { light: undefined, dark: undefined },
+    "background"
+  );
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
 
   return (
-    <ThemedView style={styles.container}>
-
-      {/* Formularios de login y register */}
-      <View style={styles.card}>
-        <View style={[styles.selector, { borderColor: colors.border }]}>
-          <SegmentedControl
-            values={["Iniciar sesión", "Registrarse"]}
-            selectedIndex={selectedIndex}
-            onChange={(event) => {
-              setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
-            }}
-            backgroundColor={colors.card}
-            tintColor="#e28700ff"
-            fontStyle={{ color: colors.text }}
-            activeFontStyle={{ color: "#fff" }}
-          />
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.container}>
+          {/* Formularios de login y register */}
+          <View style={styles.card}>
+            <View style={[styles.selector, { borderColor: colors.border }]}>
+              <SegmentedControl
+                values={["Iniciar sesión", "Registrarse"]}
+                selectedIndex={selectedIndex}
+                onChange={(event) => {
+                  setSelectedIndex(event.nativeEvent.selectedSegmentIndex);
+                }}
+                backgroundColor={colors.card}
+                tintColor="#e28700ff"
+                fontStyle={{ color: colors.text }}
+                activeFontStyle={{ color: "#fff" }}
+              />
+            </View>
+            {selectedIndex === 0 && <LoginForm />}
+            {selectedIndex === 1 && <RegisterForm />}
+          </View>
         </View>
-        {selectedIndex === 0 && <LoginForm />}
-        {selectedIndex === 1 && <ThemedText>TODO: formulario de registro</ThemedText>}
-      </View>
-    </ThemedView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -45,7 +65,7 @@ const styles = StyleSheet.create({
   card: {
     width: "100%",
     padding: 10,
-    gap: 30
+    gap: 30,
   },
   selector: {
     borderWidth: 1,
