eab794b9bb89199e31d5b6e21d9af7e22060893e
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-22 19:30:06 -0600

    ♻️  actualizar componente para que utilice el nuevo contexto de tema

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -1,21 +1,16 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
-
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from "@/constants/theme";
+import { useAppTheme } from "@/context/AppThemeContext";
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+  const { colorScheme } = useAppTheme(); // 'light' | 'dark'
+  const colorFromProps = props[colorScheme];
 
   if (colorFromProps) {
     return colorFromProps;
   } else {
-    return Colors[theme][colorName];
+    return Colors[colorScheme][colorName];
   }
 }
