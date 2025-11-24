9684495a2bd7ba050fcf61b4c0936e2809a9137a
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-24 16:08:02 -0600

    💄  refactorizar estilos de la página principal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Container } from "@/components/container";
 import { HelloWave } from "@/components/hello-wave";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
@@ -6,22 +7,25 @@ import { StyleSheet, View } from "react-native";
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <ThemedText>Bienvenido a Bitcoin Meetings</ThemedText>
-        <HelloWave />
-      </View>
+      <Container>
+        <View style={styles.headerContainer}>
+          <ThemedText>Bienvenido a Bitcoin Meetings</ThemedText>
+          <HelloWave />
+        </View>
+      </Container>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
-    flex: 1
+    paddingVertical: 10,
+    flex: 1,
+    alignItems: 'center'
   },
   headerContainer: {
-    flexDirection: 'row',
-    alignContent: 'flex-start',
-    width: '100%'
-  }
+    flexDirection: "row",
+    alignContent: "flex-start",
+    width: "100%",
+  },
 });
