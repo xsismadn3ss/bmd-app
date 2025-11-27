39e84e555b766816616d517d90f11d3acba0393b
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-27 00:52:52 -0600

    ♻️  eliminar link para la pagina de ajuses

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,7 @@
 import { Container } from "@/components/container";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { Link } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function ProfileScreen() {
   const cardDark = "#1d1d1dff";
@@ -12,15 +10,7 @@ export default function ProfileScreen() {
     <ThemedView style={styles.container}>
       <Container maxWidth={700}>
         <ThemedView darkColor={cardDark} style={styles.card}>
-          <View style={styles.cardHeader}>
-            <ThemedText style={styles.nameText}>Sathosi Nakamoto</ThemedText>
-
-            <Link href={"/settings"}>
-              <ThemedText lightColor="#494949ff" darkColor="#515151ff">
-                <MaterialIcons name={"settings"} size={21} />
-              </ThemedText>
-            </Link>
-          </View>
+          <ThemedText style={styles.nameText}>Sathosi Nakamoto</ThemedText>
           <ThemedText style={styles.emailText}>
             satoshi@example.email
           </ThemedText>
@@ -34,7 +24,7 @@ const styles = StyleSheet.create({
   container: {
     paddingBlock: 20,
     flex: 1,
-    alignItems: 'center'
+    alignItems: "center",
   },
   card: {
     borderRadius: 10,
