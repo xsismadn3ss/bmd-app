9960aae91592c4bdad4b7f4fa6c2764238cb73b9
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-27 00:51:25 -0600

    ♻️  agregar animacion fade in para el mapa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,20 @@
+import { FadeIn } from "@/components/animation/fade-in";
+import { MeetingMap } from "@/components/home/meeting-map";
 import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      {/* <MeetingMap style={styles.mapMeeting} /> */}
+      <FadeIn duration={300}>
+        <MeetingMap style={styles.mapMeeting} />
+      </FadeIn>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   mapMeeting: {
     height: "100%",
