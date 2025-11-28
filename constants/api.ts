473f1d89f08a930a7ff05c2d5a10d6205e4a046c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-28 00:09:05 -0600

    🔧  actualizar configuración de URL para API

--- constants/api.ts
+++ constants/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.API_URL
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000';
