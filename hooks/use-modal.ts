6995ed2a47b0149323377428e5bb534b0d6c6554
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-29 22:31:00 -0600

    📝  agregar documentación al hook de modal

--- hooks/use-modal.ts
+++ hooks/use-modal.ts
@@ -1,6 +1,16 @@
 import { useState } from "react";
 
-export function useModal() {
+interface ModalHook {
+  /** indica si el modal está visible */
+  isVisible: boolean;
+  /** función para abrir el modal */
+  openModal: () => void;
+  /** función para cerrar el modal */
+  closeModal: () => void;
+}
+
+/** Hook para manejar la visibilidad de un modal */
+export function useModal(): ModalHook {
   const [isVisible, setIsVisible] = useState(false);
   const openModal = () => setIsVisible(true);
   const closeModal = () => setIsVisible(false);
